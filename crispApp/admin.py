d88c4cf6df1a7870735e9a037f009e335d92eb48
topCodegeek <topkarishaan@gmail.com>
2024-06-20 12:41:21 +0530

    Created Todo Model

--- crispApp/admin.py
+++ crispApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import UserProfile, Todo
 # Register your models here.
+
+class TodoAdmin(admin.ModelAdmin):
+     readonly_fields = ('created',)
+admin.site.register(UserProfile)
+admin.site.register(Todo, TodoAdmin)
--- crispApp/models.py
+++ crispApp/models.py
@@ -14,3 +14,14 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    memo = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    date_completed = models.DateTimeField(null=True, blank=True)
+    important = models.BooleanField(default=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
