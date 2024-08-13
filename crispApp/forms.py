aed1ababc558c83e3ba7eff7a5e72d73cafee387
topCodegeek <topkarishaan@gmail.com>
2024-08-13 19:02:49 +0530

    Removed deadline

--- crispApp/forms.py
+++ crispApp/forms.py
@@ -4,7 +4,7 @@ from .models import Todo
 class TodoForm(ModelForm):
      class Meta:
           model = Todo
-          fields = ['title', 'memo', 'important','visibility','complete_by']
+          fields = ['title', 'memo', 'important','visibility']
           widgets = {
             'complete_by': DateInput(attrs={'type': 'date'}),
           }
